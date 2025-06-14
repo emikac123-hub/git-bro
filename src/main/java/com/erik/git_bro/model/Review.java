ceb8d96cebf530b4094552b4855877c0ba918c0c
Erik Mikac <5546184+HankSchrader@users.noreply.github.com>
2025-06-14 15:49:00 -0400

    changed columns to CLOBS to fit the diff_content and the feedback

--- src/main/java/com/erik/git_bro/model/Review.java
+++ src/main/java/com/erik/git_bro/model/Review.java
@@ -5,8 +5,10 @@
 
 package com.erik.git_bro.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import jakarta.persistence.NamedEntityGraph;
 import lombok.Data;
 
@@ -23,7 +25,11 @@ public class Review {
     private String reviewId; // PR-Number + TimeStamp
     private String pullRequestId;
     private String filePath;
+    @Lob
+    @Column(name ="diff_content", columnDefinition = "CLOB")
     private String diffContent;
+    @Lob
+    @Column(name ="feedback", columnDefinition = "CLOB")
     private String feedback; // AI Generated Comment
     private String createdAt;
 }
