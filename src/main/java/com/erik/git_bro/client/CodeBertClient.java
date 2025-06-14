f8ba9b8423d09fc601b8c7f98df788a3319e8906
Erik Mikac <5546184+HankSchrader@users.noreply.github.com>
2025-06-14 08:11:10 -0400

    Added my secret into github. Fixed action.

--- src/main/java/com/erik/git_bro/client/CodeBertClient.java
+++ src/main/java/com/erik/git_bro/client/CodeBertClient.java
@@ -2,8 +2,10 @@ package com.erik.git_bro.client;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import jakarta.annotation.PostConstruct;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -12,29 +14,32 @@ import okhttp3.Response;
 
 @Component
 public class CodeBertClient {
+    @Value("${huggingface.api.token}")
+    private String huggingfaceToken;
     private final OkHttpClient client = new OkHttpClient();
     private static final String API_URL = "https://api-inference.huggingface.co/models/microsoft/codebert-base";
-    private static final String API_TOKEN = "";
 
     public String analyzeCode(String codeSnippet) throws IOException {
         final var body = RequestBody.create(
-            "{\"inputs\": \"" + codeSnippet + "\"}",
-            MediaType.parse("application/json")
-        );
+                "{\"inputs\": \"" + codeSnippet + "\"}",
+                MediaType.parse("application/json"));
 
         final Request request = new Request.Builder()
-            .url(API_URL)
-            .header("Authorization", "Bearer " + API_TOKEN)
-            .post(body)
-            .build();
+                .url(API_URL)
+                .header("Authorization", "Bearer " + this.huggingfaceToken)
+                .post(body)
+                .build();
 
         try (Response response = client.newCall(request).execute()) {
-            if(!response.isSuccessful()) {
+            if (!response.isSuccessful()) {
                 throw new IOException("Unexpected code " + response);
             }
             return response.body().string();
         }
     }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Loaded HuggingFace Token: " + huggingfaceToken);
+    }
 }
-
-
